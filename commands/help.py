a0f436e13bcfbe4dced964e2cf13132eab063c7a
withoutwyatt <wyatt.ruehl@mailbox.org>
2026-03-23 08:42:56 +0100

    adding more robust html structure to help command for better formatting

--- commands/help.py
+++ commands/help.py
@@ -1,16 +1,18 @@
 from nio import AsyncClient, RoomMessageText, MatrixRoom
 
+import templates.help
+
 
 async def help_command(room: MatrixRoom, event: RoomMessageText, client: AsyncClient) -> None:
     if event.body.startswith("!wordle") and "help" in event.body:
-        formatted_body = ("<h1 data-md=\"#\">What can I do?</h1>"
-                        "I am the Wordle bot, obviously you can play Wordle with me.<br>"
-                        "You can use the following commands:<br>"
-                        "<strong data-md=\"**\">start:</strong> register for the game<br>"
-                        "<strong data-md=\"**\">guess:</strong> get a overview of your guesses of today<br>"
-                        "<strong data-md=\"**\">guess</strong> <i data-md=\"*\">{word}</i><strong data-md=\"**\">:</strong> take a guess at what today's Wordle might be<br>"
-                        "<strong data-md=\"**\">score:</strong> get a overview of the players scores<br>"
-                        "<strong data-md=\"**\">date</strong> <i data-md=\"*\">{yyyy-mm-dd}</i><strong data-md=\"**\">:</strong> get the Wordle for a specific date<br>"
+        body = ("# What can I do?"
+                        "I am the Wordle bot, obviously you can play Wordle with me.\n"
+                        "You can use the following commands:\n"
+                        "**start:** register for the game\n"
+                        "**guess:** get a overview of your guesses of today\n"
+                        "**guess** *{word}* **:** take a guess at what today's Wordle might be\n"
+                        "**score:** get a overview of the players scores\n"
+                        "**date** *{yyyy-mm-dd}* **:** get the Wordle for a specific date\n"
                         "Example: !wordle start")
 
         await client.room_send(
@@ -18,16 +20,9 @@ async def help_command(room: MatrixRoom, event: RoomMessageText, client: AsyncCl
             message_type="m.room.message",
             content={
                 "msgtype": "m.text",
-                "body": formatted_body
-                .replace("<strong data-md=\"**\">", "**")
-                .replace("</strong>", "**")
-                .replace("<h1 data-md=\"#\">", "# ")
-                .replace("</h1>", "")
-                .replace("<br>", "\n")
-                .replace("<i data-md=\"*\">", "*")
-                .replace("</i>", "*"),
+                "body": body,
                 "format": "org.matrix.custom.html",
-                "formatted_body": formatted_body,
+                "formatted_body": templates.help.help_template,
                 "m.relates_to": {
                     "rel_type": "m.reply",
                     "event_id": event.event_id,
